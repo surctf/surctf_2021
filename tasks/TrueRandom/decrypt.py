b7ee9f3cb937aacaa83bf8501be1e6c34d371a3b
Maxim Zakazchik <zakazchikm@gmail.com>
2021-03-23 19:45:23 +0500

    Update decrypt.py

--- tasks/TrueRandom/decrypt.py
+++ tasks/TrueRandom/decrypt.py
@@ -11,9 +11,9 @@ IMG_H = enc_img.shape[0]
 dec_img = np.zeros(enc_img.shape, dtype="uint8")
 
 for i in range(IMG_H):
-	shuffle_map = np.arange(IMG_W)
-	np.random.shuffle(shuffle_map)
-	for j in range(IMG_W):
-		dec_img[i, shuffle_map[j]] = enc_img[i, j]
+    shuffle_map = np.arange(IMG_W)
+    np.random.shuffle(shuffle_map)
+    for j in range(IMG_W):
+        dec_img[i, shuffle_map[j]] = enc_img[i, j]
 
 cv2.imwrite("dec.png", dec_img)
