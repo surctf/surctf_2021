ccad7a2affe52c9ae11a5b73a785ad9171916e57
Maxim Zakazchik <zakazchikm@gmail.com>
2021-04-04 08:12:58 +0500

    Update decrypt.py

--- tasks/TrueRandom/decrypt.py
+++ tasks/TrueRandom/decrypt.py
@@ -1,7 +1,9 @@
 import numpy as np
 import cv2
 
-np.random.seed(1337)
+def get_true_random_number():
+    np.random.seed(np.random.randint(1000000))
+    return np.random.randint(10000000)
 
 enc_img = cv2.imread("enc.png")
 
@@ -9,10 +11,13 @@ IMG_W = enc_img.shape[1]
 IMG_H = enc_img.shape[0]
 
 dec_img = np.zeros(enc_img.shape, dtype="uint8")
+np.random.seed(IMG_W * IMG_H * 666)
 
 for i in range(IMG_H):
+    np.random.seed(get_true_random_number())
     shuffle_map = np.arange(IMG_W)
     np.random.shuffle(shuffle_map)
+
     for j in range(IMG_W):
         dec_img[i, shuffle_map[j]] = enc_img[i, j]
 
