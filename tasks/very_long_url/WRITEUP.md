246270b1b850b2cb311f71cf335272abe0bf9d84
pigadoor <30825714+pigadoor@users.noreply.github.com>
2021-10-25 05:19:35 +0300

    Add files via upload

---
+++ tasks/very_long_url/WRITEUP.md
@@ -0,0 +1,5 @@
+Сайт шифрует сообщение в base4, добаляя похожие похожие символы с разным Юникодом
+
+Пару раз дешефруем и получаем url: http://surctf_oooo_thiiiiiiiiiiiiiiis_iiiiiiiiiiiiiis_looooooooooooong/
+
+`Флаг: surctf_oooo_thiiiiiiiiiiiiiiis_iiiiiiiiiiiiiis_looooooooooooong`
---
+++ tasks/very_long_url/src/index.js
@@ -0,0 +1,70 @@
+const input = document.getElementById("input-url")
+const output = document.getElementById("output-url")
+
+// init
+input.addEventListener("keydown", e => e.code == "Enter" ? oooify() : null)
+
+// make tooltips
+tippy(
+    "#copy-button",
+    {
+        content: "Скопировано",
+        trigger: "клик",
+        animation: "shift-away-subtle"
+    }
+)
+
+function oooify() {
+
+    if (window.inBlinking) return
+
+    try {
+        new URL(input.value)
+    } catch (e) {
+
+        window.inBlinking = true
+
+        input.style.opacity = 1
+        input.disabled = true
+
+        let oldValue = input.value
+        input.value = "Неправильный URL! НЕЕЕЕЕЕЕЕТ!"
+
+        let times = 0
+        let i = setInterval(async () => {
+            input.style.opacity = (parseInt(input.style.opacity) + 1) % 2
+            if (++times == 6) {
+                clearInterval(i)
+                input.value = oldValue
+                input.disabled = false
+                input.focus()
+                inBlinking = false
+            }
+        }, 100)
+        return
+    }
+
+    const domain = location.host
+
+    let url = new OOO().encodeUrl(input.value.trim())
+    url = `${location.protocol}//${domain}/${url}`
+
+    document.getElementById("output-div").classList.remove("d-none")
+
+    output.innerHTML = url
+    output.setAttribute("href", url)
+
+    input.value = ""
+}
+
+function copy() {
+    const el = document.createElement('textarea');
+    el.value = output.innerHTML;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+
+    setTimeout(() => tippy.hideAll(), 2000)
+
+};
