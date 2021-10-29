8d0dd3ba5d6f6d093527c229e4e6c56d9f761309
richkats <katsur.serg@gmail.com>
2021-10-29 15:34:27 +0500

    Should work

---
+++ tasks/very_long_url/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=1010)
